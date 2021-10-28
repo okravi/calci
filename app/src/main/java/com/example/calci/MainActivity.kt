d5bcad2163410f3635aca8f1974af69dcf7d7d8f
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-10-28 21:57:45 +0300

    progress update

--- app/src/main/java/com/example/calci/MainActivity.kt
+++ app/src/main/java/com/example/calci/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.calci
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onDigit(view: View){
-    Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
+        tvInput.append((view as Button).text)
     }
+
+    fun onCLR(view: android.view.View) {
+        tvInput.text = ""
+    }
+
 }
