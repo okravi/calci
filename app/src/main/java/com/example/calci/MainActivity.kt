3b7cce9baec126bb6ae6900356eb738c5a94b838
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-10-30 19:18:04 +0300

    progress update

--- app/src/main/java/com/example/calci/MainActivity.kt
+++ app/src/main/java/com/example/calci/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.example.calci.databinding.ActivityMainBinding
+import java.lang.ArithmeticException
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,20 +21,76 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    var lastDigit: Boolean = false
     fun onDigit(view: View){
        binding.tvInput.append((view as Button).text)
-
+       lastDigit = true
     }
 
     fun onCLR (view: View){
         binding.tvInput.text = ""
+        operatorAdded = false
+
+        dotPresent = false
+
+    }
+    var dotPresent: Boolean = false
+
+    fun onDecimal(view: View) {
+        if (!dotPresent && lastDigit) {
+            binding.tvInput.append(".")
+            dotPresent = true
+            lastDigit = false
+            }
+        /*if (!dotPresent && lastDigit && operatorAdded){
+            binding.tvInput.append(".")
+            dotPresent = true
+            lastDigit = false
+            }*/
+        //    operatorAdded = false
 
     }
 
-    fun onDecimal(view: View) {
-        if ("." !in (binding.tvInput.text) && (binding.tvInput.text.isNotEmpty())) {
-            binding.tvInput.append(".")
+    var operatorAdded: Boolean = false
+    var operationToDo = "x"
+    fun onOperator(view: android.view.View) {
+        if (!operatorAdded && binding.tvInput.text.isNotEmpty()){
+            binding.tvInput.append((view as Button).text)
+            operationToDo = (view as Button).text.toString()
+            operatorAdded = true
+            dotPresent = false
+            lastDigit = false
+
         }
     }
 
+    fun onEquals(view: android.view.View) {
+        if (lastDigit) {
+            var tvValue = binding.tvInput.text.toString()
+
+            try {
+
+
+                val firstNumber = tvValue.substringBeforeLast(operationToDo)
+                val secondNumber = tvValue.substringAfterLast(operationToDo)
+
+                when (operationToDo){
+
+                    "-" -> binding.tvInput.text = (firstNumber.toDouble() - secondNumber.toDouble()).toString()
+                    "+" -> binding.tvInput.text = (firstNumber.toDouble() + secondNumber.toDouble()).toString()
+                    "/" -> binding.tvInput.text = (firstNumber.toDouble() / secondNumber.toDouble()).toString()
+                    "*" -> binding.tvInput.text = (firstNumber.toDouble() * secondNumber.toDouble()).toString()
+
+
+                }
+                //Toast.makeText(this@MainActivity, "a: $firstNumber, b: $secondNumber", Toast.LENGTH_SHORT).show()
+
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+            operatorAdded = false
+
+        }
+
+    }
 }
