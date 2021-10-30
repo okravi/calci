cb87b93764ee12910a8ea0b32be43efd70016886
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-10-30 21:13:50 +0300

    progress update

--- app/src/main/java/com/example/calci/MainActivity.kt
+++ app/src/main/java/com/example/calci/MainActivity.kt
@@ -4,10 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import com.example.calci.databinding.ActivityMainBinding
 import java.lang.ArithmeticException
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,9 +21,11 @@ class MainActivity : AppCompatActivity() {
 
 
     var lastDigit: Boolean = false
+
     fun onDigit(view: View){
        binding.tvInput.append((view as Button).text)
        lastDigit = true
+
     }
 
     fun onCLR (view: View){
@@ -39,17 +39,12 @@ class MainActivity : AppCompatActivity() {
 
     fun onDecimal(view: View) {
         if (!dotPresent && lastDigit) {
-            binding.tvInput.append(".")
-            dotPresent = true
-            lastDigit = false
-            }
-        /*if (!dotPresent && lastDigit && operatorAdded){
-            binding.tvInput.append(".")
-            dotPresent = true
-            lastDigit = false
-            }*/
-        //    operatorAdded = false
 
+            binding.tvInput.append(".")
+            dotPresent = true
+            lastDigit = false
+
+            }
     }
 
     var operatorAdded: Boolean = false
@@ -66,28 +61,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun removeExcessiveZeroes(value: String) : String{
-        //var shortenedResult = value.substring(0, 11)
+
         var shortenedResult = value
         when (value.length) { !in 0..11 -> shortenedResult = value.substring(0, 11)}
 
-
-        Toast.makeText(this@MainActivity, "Trying to shorten: $shortenedResult", Toast.LENGTH_SHORT).show()
-        //TimeUnit.SECONDS.sleep(5L)
         while (shortenedResult.endsWith("0")) {
-            shortenedResult = shortenedResult.substring(0, shortenedResult.length -1)
+            shortenedResult = shortenedResult.dropLast(1)
+        }
 
-            Toast.makeText(this@MainActivity, "Shortening: $shortenedResult", Toast.LENGTH_SHORT).show()
-        }
         if (shortenedResult.endsWith(".")){
-            shortenedResult = shortenedResult.substring(0, shortenedResult.length -1)
+            shortenedResult = shortenedResult.dropLast(1)
         }
-        //    shortenedResult = shortenedResult
+
         return shortenedResult
     }
 
     fun onEquals(view: android.view.View) {
         if (lastDigit) {
-            var tvValue = binding.tvInput.text.toString()
+            val tvValue = binding.tvInput.text.toString()
 
             try {
 
@@ -103,15 +94,13 @@ class MainActivity : AppCompatActivity() {
                     "/" -> result = (firstNumber.toDouble() / secondNumber.toDouble()).toString()
                     "*" -> result = (firstNumber.toDouble() * secondNumber.toDouble()).toString()
 
-
-
                 }
                 binding.tvInput.text = removeExcessiveZeroes(result)
-                //Toast.makeText(this@MainActivity, "a: $firstNumber, b: $secondNumber", Toast.LENGTH_SHORT).show()
 
             } catch (e: ArithmeticException) {
                 e.printStackTrace()
-            }
+                }
+
             operatorAdded = false
 
         }
